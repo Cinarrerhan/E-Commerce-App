773fb55cd41aec78b2b28304a5abb7630c8ae60f
Cinarrerhan <cinarerhan94@gmail.com>
2022-08-29 19:01:10 +0300

    update category

--- BACKEND/routes/categories.js
+++ BACKEND/routes/categories.js
@@ -8,7 +8,18 @@ router.get(`/`, async (req, res) => {
   if (!categoryList) {
     res.status(500).json({ success: false });
   }
-  res.send(categoryList);
+
+  res.status(200).send(categoryList);
+});
+
+router.get("/:id", async (req, res) => {
+  const category = await Category.findById(req.params.id);
+
+  if (!category) {
+    res.status(500).json({ message: "Not found" });
+  }
+
+  res.status(200).send(category);
 });
 
 router.post("/", async (req, res) => {
@@ -20,6 +31,24 @@ router.post("/", async (req, res) => {
   category = await category.save();
 
   if (!category) return res.status(404).send("the category cannot be created!");
+
+  res.send(category);
+});
+
+router.put("/:id", async (req, res) => {
+  const category = await Category.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      icon: req.body.icon,
+      color: req.body.color,
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!category) return res.status(404).send("the category cannot be updated!");
 
   res.send(category);
 });
