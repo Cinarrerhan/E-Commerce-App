d8f4e038afeb7f950d72ed1db536a7185c375c81
Cinarrerhan <cinarerhan94@gmail.com>
2022-09-01 00:52:02 +0300

    update user

--- BACKEND/routes/users.js
+++ BACKEND/routes/users.js
@@ -41,4 +41,39 @@ router.post("/", async (req, res) => {
   res.send(user);
 });
 
+router.put("/:id", async (req, res) => {
+  const userExist = await User.findById(req.params.id);
+
+  let newPassword;
+
+  if (req.body.password) {
+    newPassword = bcrypt.hashSync(req.body.password, 10);
+  } else {
+    newPassword = userExist.passwordHash;
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+      color: req.body.color,
+      passwordHash: newPassword,
+      phone: req.body.phone,
+      isAdmin: req.body.isAdmin,
+      apartment: req.body.apartment,
+      zip: req.body.zip,
+      city: req.body.city,
+      country: req.body.country,
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!user) return res.status(404).send("the user cannot be updated!");
+
+  res.send(user);
+});
+
 module.exports = router;
