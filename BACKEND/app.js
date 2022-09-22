537a54d68c85ba1f9d5cd7d49119d43784a77599
Cinarrerhan <cinarerhan94@gmail.com>
2022-09-22 22:08:11 +0300

    protecting the api

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -5,19 +5,21 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv/config");
-
+const authJwt = require("./helpers/jwt");
 app.use(cors());
 app.options("*", cors());
 
 //middleware
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
+app.use(authJwt());
 
 //Routes
 const categoriesRoutes = require("./routes/categories");
 const productsRoutes = require("./routes/products");
 const usersRoutes = require("./routes/users");
 const ordersRoutes = require("./routes/orders");
+
 
 const api = process.env.API_URL;
 
